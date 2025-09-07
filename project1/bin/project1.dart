74031f4c594fc9c8f98fee58a6f524a47ff88a3e
maisyadhia <maisyaazzahra05@gmail.com>
2025-09-07 17:13:10 +0700

    belajar komen

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -1,5 +1,10 @@
 import 'package:project1/project1.dart' as project1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project1.calculate()}!');
+  //print('Hello world: ${project1.calculate()}!');
+  //satu line comment
+  /*
+  multi line comment
+  banyak line comment
+  */
 }
